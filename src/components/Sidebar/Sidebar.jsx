a7ff45c656c3937fe6f9aac2e97ec35ef24afafd
Rownokk <mowmita878@gmail.com>
2024-08-31 16:37:46 +0600

    init

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+      <img className='menu' src={assets.menu_icon} alt="" />
+      <div className="new-chat">
+        <img src={assets.plus_icon} alt="" />
+        <p>New Chat</p>
+      </div>
+      <div className="recent">
+        <p className='recent-title'>Recent</p>
+        <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react...</p>
+        </div>
+      </div>
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="" />
+            <p>Help</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
